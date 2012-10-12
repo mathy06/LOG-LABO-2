c0bcc5fab13d3bb0f56f7723543f25cea5308b45
Patrice Robitaille <web.probitaille@gmail.com>
2012-10-12 14:30:44 -0400

    Ajout de l'itérateur

---
+++ src/clientforme/Iterateur.java
@@ -0,0 +1,8 @@
+package clientforme;
+
+public interface Iterateur {
+	public boolean possedeSuivant();
+	public Object suivant();
+	public boolean possedePrecedant();
+	public Object precedant();
+}
