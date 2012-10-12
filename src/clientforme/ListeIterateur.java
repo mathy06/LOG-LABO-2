5d484df47706a6a202cfc7c05da6f19ee6777979
Patrice Robitaille <web.probitaille@gmail.com>
2012-10-12 15:47:50 -0400

    MaJ iterator

--- src/clientforme/ListeIterateur.java
+++ src/clientforme/ListeIterateur.java
@@ -3,45 +3,37 @@ package clientforme;
 public class ListeIterateur implements Iterateur {
 
 	private ListeForme listeforme;
-	private int tailleListe;
-	private int position;
+	private Noeud courant;
 	
 	public ListeIterateur(ListeForme liste){
 		listeforme = liste;
-		tailleListe = listeforme.obtenirTaille();
-		position = 0;
 	}
 	
 	@Override
 	public boolean possedePrecedant() {
-		if (position > 0)
-			return true;
-		else
-			return false;
+		
+		return(courant.obtenirPrecedant() != null);
+		
 	}
 
 	@Override
 	public boolean possedeSuivant() {
-		if (position < tailleListe)
-			return true;
-		else
-			return false;
+
+		return(courant.obtenirPrecedant() != null);
+	
 	}
 
 	@Override
 	public Noeud precedant() {
 		
+		if(courant == null){
+			courant = listeforme.getQueue();
+		}
+		
 		if (this.possedePrecedant()){
-
-			Noeud courant = listeforme.getQueue();
-			for(int i = position; i >= 0; i--)
-			{
-				if(courant.obtenirPrecedant() == null)
-					return null;
-				
-				courant = courant.obtenirPrecedant();
-			}
-			position--;
+					
+			courant = courant.obtenirPrecedant();
+			
 			return courant;
 			
 		}else{
@@ -52,17 +44,14 @@ public class ListeIterateur implements Iterateur {
 	@Override
 	public Noeud suivant() {
 		
+		if(courant == null){
+			courant = listeforme.getSommet();
+		}
+		
 		if (this.possedeSuivant()){
 
-			Noeud courant = listeforme.getSommet();
-			for(int i = 0; i <= position; i++)
-			{
-				if(courant.obtenirSuivant() == null)
-					return null;
-				
-				courant = courant.obtenirSuivant();
-			}
-			position++;
+			courant = courant.obtenirSuivant();
+		
 			return courant;
 			
 		}else{
